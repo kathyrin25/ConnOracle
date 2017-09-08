50be85426102c2c00d071c2b77b5d9f44a8140ff
kathy.peng <kathyrin25@gmail.com>
2017-09-08 14:23:50 +0800

    Create a new Model

---
+++ ConnOracle/Models/INVLinesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnOracle.Models
+{
+    public class INVLinesModel
+    {
+        public string invoice_num { get; set; }
+        public int line_number { get; set; }
+
+        public int amount { get; set; }
+
+        public string account_segment { get; set; }
+
+        public string description { get; set; }
+    }
+}
